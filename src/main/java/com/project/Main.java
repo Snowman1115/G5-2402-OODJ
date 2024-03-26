24550c8ab79fe092457b1175673c473a75954eb1
chj <tp065304@mail.apu.edu.my>
2024-03-26 15:48:35 +0800

    G5-2402-OODJ - Project File Setup

---
+++ src/main/java/com/project/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Function To Run The Program
+ * @author CHAN HOONG JIAN
+ * @version 1.0, Last edited on 2024-03-26
+ * @since 2024-03-26
+ */
+
+package com.project;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
+}

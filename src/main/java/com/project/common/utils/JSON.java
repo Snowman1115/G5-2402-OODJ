a6e606fc058af6feaba268802c9434a0ddfe25d7
Au Yik Hoe <yikhoe@softwell.asia>
2024-03-27 12:58:16 +0800

    update json handler

--- src/main/java/com/project/common/utils/JSON.java
+++ src/main/java/com/project/common/utils/JSON.java
@@ -5,6 +5,18 @@
  * @since 2024-03-27
  */
 package com.project.common.utils;
-public class JSON {
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JSON {
+    private JSONArray users = (JSONArray) new JSONParser().parse(new FileReader("users.txt"));
+
+    public JSON() throws IOException, ParseException {
+
+    }
 }

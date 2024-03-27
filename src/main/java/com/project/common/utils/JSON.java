b697320aaa6365048b7d8f0d7ff96892eca8508e
Au Yik Hoe <yikhoe@softwell.asia>
2024-03-27 12:36:53 +0800

    Added json dependency. Created JSON handler utils

---
+++ src/main/java/com/project/common/utils/JSON.java
@@ -0,0 +1,10 @@
+/**
+ * Utils - JSON Object handler
+ * @author AU YIK HOE
+ * @version 1.0, Last edited on 2024-03-27
+ * @since 2024-03-27
+ */
+package com.project.common.utils;
+public class JSON {
+
+}

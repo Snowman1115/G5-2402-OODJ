3166aabe62813232346a6788d210df607545ce32
YikHoe <ayh120103@gmail.com>
2024-03-28 14:19:05 +0800

    file structure update

--- src/main/java/com/project/common/utils/JsonHandler.java
+++ src/main/java/com/project/common/utils/JsonHandler.java
@@ -1,26 +1,27 @@
 /**
  * Utils - JSON Object handler
  * @author AU YIK HOE
- * @version 1.0, Last edited on 2024-03-27
+ * @version 1.0, Last edited on 2024-03-28
  * @since 2024-03-27
  */
 package com.project.common.utils;
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-public class json_handler {
+@Slf4j
+public class JsonHandler {
     private JSONArray json_array;
     private JSONObject json_object;
-    public json_handler(String json_text) {
+    public JsonHandler(String json_text) {
         try {
             json_array = (JSONArray) new JSONParser().parse(json_text);
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
     }
-    public json_handler(Object object) {
+    public JsonHandler(Object object) {
         json_object = (JSONObject) object;
     }
     // ======== JSON array methods ========
@@ -34,10 +35,49 @@ public class json_handler {
 
     /**
      * Get specific object from json array
+     * @param index
      * @return object
      */
     public Object getObject(int index) {
         return  json_array.get(index);
+    }
+
+    /**
+     * Update specific object from json array
+     * @param objectId
+     * @param attribute
+     * @param value
+     */
+    public void update(int objectId, String attribute, String value) {
+        for (Object o : json_array) {
+            JSONObject uo = (JSONObject) o;
+            int uoId = Integer.parseInt(uo.get("id").toString());
+            if (uoId == objectId) {
+                json_array.remove(uo);
+
+                switch (attribute) {
+                    case "username" -> {
+                        uo.replace("username", value);
+                        json_array.add(uo);
+                    }
+                    case "first_name" -> {
+                        uo.replace("first_name", value);
+                        json_array.add(uo);
+                    }
+                    case "last_name" -> {
+                        uo.replace("last_name", value);
+                        json_array.add(uo);
+                    }
+                    default -> {
+                        log.info("(" + attribute + ") Attribute not found.");
+                    }
+                }
+
+                break;
+            }
+        }
+
+
     }
 
     // ======== JSON object methods ========
@@ -69,9 +109,7 @@ public class json_handler {
             try {
                 return Integer.parseInt(val);
             } catch (NumberFormatException e) {
-                System.out.println("************************************************************************\n\n" +
-                        "Invalid method getInt(), Value contains non-numeric characters: " + val +
-                        "\n\n************************************************************************");
+                log.info("Invalid method getInt(), Value contains non-numeric characters: " + val);
                 return null;
             }
         }
@@ -91,9 +129,7 @@ public class json_handler {
             try {
                 return Double.parseDouble(val);
             } catch (NumberFormatException e) {
-                System.out.println("************************************************************************\n\n" +
-                        "Invalid method getDouble(), Value contains non-numeric characters: " + val +
-                        "\n\n************************************************************************");
+                log.info("Invalid method getDouble(), Value contains non-numeric characters: " + val);
                 return null;
             }
         }

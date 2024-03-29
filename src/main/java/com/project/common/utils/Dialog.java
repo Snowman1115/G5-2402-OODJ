367d6cd33a02f85d6675616597eff06c95151761
Jin1227 <jinxun1227@gmail.com>
2024-03-29 15:54:34 +0800

    testing

--- src/main/java/com/project/common/utils/Dialog.java
+++ src/main/java/com/project/common/utils/Dialog.java
@@ -23,6 +23,7 @@ public class Dialog {
      * Prompt Error Dialog
      * @param message
      */
+    //helo lengzaii, jin xun here
     public static void ErrorDialog(String message){
         ImageIcon eicon = new ImageIcon(ERRORICON);
         JOptionPane.showMessageDialog(null,message, DEFAULTERRORTITLE, JOptionPane.ERROR_MESSAGE,eicon);
